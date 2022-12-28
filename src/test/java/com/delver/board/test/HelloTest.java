57aae8a3f7fc5672e14229c43a1905b5f7ba98f9
delver <delvering17@gmail.com>
2022-12-28 17:08:12 +0900

    Chore: Test에서 @Slf4j 적용을 위한 의존 추가

---
+++ src/test/java/com/delver/board/test/HelloTest.java
@@ -0,0 +1,15 @@
+package com.delver.board.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class HelloTest {
+
+
+    @Test
+    public void Hello() throws Exception {
+        log.info("Hello");
+
+    }
+}
