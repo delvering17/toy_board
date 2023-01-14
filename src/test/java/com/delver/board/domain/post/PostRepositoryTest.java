1eb6b5447a4935ca9c6b1c78d5d829bc68e41dad
delver <delvering17@gmail.com>
2023-01-14 20:39:42 +0900

    Test: 페이징 테스트

--- src/test/java/com/delver/board/domain/post/PostRepositoryTest.java
+++ src/test/java/com/delver/board/domain/post/PostRepositoryTest.java
@@ -3,6 +3,7 @@ package com.delver.board.domain.post;
 import com.delver.board.domain.member.JoinRoot;
 import com.delver.board.domain.member.Member;
 import com.delver.board.domain.member.Role;
+import com.delver.board.web.constant.PostConst;
 import com.delver.board.web.controller.dto.MemberSaveRequestDto;
 import com.delver.board.web.controller.dto.PostSaveRequestDto;
 import jakarta.persistence.EntityManager;
@@ -31,6 +32,7 @@ class PostRepositoryTest {
         // given
         Member member = createMember();
         Post post = createPostEntity(member);
+
         // when
         postRepository.save(post);
 
@@ -59,6 +61,44 @@ class PostRepositoryTest {
 
     }
 
+    @Test
+    public void 페이징() throws Exception {
+        // given
+        Member member = createMember();
+        for (int i = 1; i <= 30; i++) {
+            Post post = createPostEntity("제목" + i, "내용" + i, "카테고리", member);
+            postRepository.save(post);
+        }
+
+        // when
+        List<Post> posts = postRepository.findPage(0, PostConst.POST_LIMIT);
+
+        // then
+        assertThat(posts.size()).isEqualTo(10);
+        assertThat(posts.get(0).getTitle()).isEqualTo("제목1");
+        assertThat(posts.get(0).getContent()).isEqualTo("내용1");
+
+    }
+
+    @Test
+    public void 전체_페이지_카운트() throws Exception {
+        // given
+        Member member = createMember();
+        int countPage = 59;
+        for (int i = 1; i <= 59; i++) {
+            Post post = createPostEntity("제목" + i, "내용" + i, "카테고리", member);
+            postRepository.save(post);
+        }
+
+        // when
+        Long resultCount = postRepository.countPost();
+
+        // then
+        assertThat(resultCount).isEqualTo(countPage);
+
+    }
+
+
 
     @Test
     public void delete() throws Exception {
@@ -81,6 +121,8 @@ class PostRepositoryTest {
     private Member createMember() {
         MemberSaveRequestDto dto = MemberSaveRequestDto.createLocalMember()
                 .userName("delver")
+                .password("password")
+                .passwordConfirm("password")
                 .email("delvering17@gmail.com")
                 .role(Role.USER)
                 .joinRoot(JoinRoot.LOCAL)
@@ -88,6 +130,15 @@ class PostRepositoryTest {
         Member member = dto.toEntity();
         em.persist(member);
         return member;
+    }
+
+    private static Post createPostEntity(String title, String content, String category, Member member) {
+        return PostSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .category(category)
+                .build()
+                .toEntity(member);
     }
 
     private static Post createPostEntity(Member member) {
